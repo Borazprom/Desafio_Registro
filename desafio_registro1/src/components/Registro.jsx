529f1008ef17fc97d699ea4910f7d34c07457842
Borazprom <felipe.borbaran@gmail.com>
2024-03-30 18:26:20 -0300

    Iniciando proyecto

---
+++ desafio_registro1/src/components/Registro.jsx
@@ -0,0 +1,16 @@
+import SocialButton from "./SocialButton"
+import Formulario from "./Formulario"
+import Alert from "./Alert"
+
+const Registro = () => {
+    return (
+        <>
+        //No se como enviar los iconos
+        <SocialButton facebook=""  />
+        <Formulario/>
+        <Alert />
+        </>
+    )
+}
+
+export default Registro
