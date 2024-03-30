5db249b184eb09eecc937b843d0a392e02b23519
Borazprom <felipe.borbaran@gmail.com>
2024-03-30 18:29:22 -0300

    Modificando SocialButton

---
+++ desafio_registro1/src/components/SocialButton.jsx
@@ -0,0 +1,9 @@
+const SocialButton = ({facebook, github, linkedin}) => {
+    return (
+        <>
+            <button><i></i></button>
+        </>
+    )
+}
+//No se como ingresar los iconos
+export default SocialButton;
